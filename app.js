6a2a606edeca0603764a7c211178442fb26cab2d
JayHarvie <Jayharv14@gmail.com>
2024-10-03 14:19:04 -0300

    added cors

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cors());
+
+
 app.use('/api/contacts', contactsRouter);
 
 app.listen(port, () => {
